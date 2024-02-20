0d2ef5b78eeabf6381799e70900db3a3bc5e635f
2021-08-24 <bmach@xena23.cs.trinity.edu>
2024-02-20 12:34:00 -0600

    2/20 push

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -17,8 +17,18 @@ class Application @Inject()(cc: ControllerComponents) extends AbstractController
     Ok(views.html.test213(SharedMessages.itWorks))
   }
 
-  def getNBATable = Action {
-    Ok(views.html.nbaTable(NBATeams.tableHeader, NBATeams.tableData))
+  def getNBATable(col: String) = Action {
+    val colNum = NBATeams.tableHeader.indexOf(col)
+    println(col)
+    Ok(views.html.nbaTable(NBATeams.tableHeader, NBATeams.tableData.sortBy(row => row(colNum))))
+  }
+
+  def getInfo = Action {
+      Ok(views.html.favCol())
+  }
+
+  def color(name: String, color: String) = Action {
+      Ok(s"$name's favorite color is $color")
   }
 
 }
